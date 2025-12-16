100c1b4e2fa4d1284b2b1548414a90fbb123f29e
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-16 16:56:09 -0300

    feat: remoção de arquivos para recapitular o conteúdo

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
-
-public class Carro {
-    public String Nome;
-    public String Modelo;
-    public int ano;
-}
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
+++
@@ -1,8 +0,0 @@
-package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
-
-public class Estudante {
-    public String Pessoa;
-    public int Idade;
-    public char Sexo;
-    public double altura;
-}
