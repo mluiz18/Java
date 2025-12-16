bf10f8ced77359dcbb8c03ec9c139f346d8107b9
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-16 17:26:37 -0300

    feat: adiciona exemplo da aula 02 sobre tipos primitivos

---
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 18;
+        System.out.println("A idade é: " + idade + " anos");
+    }
+}
