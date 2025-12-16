af51bfef64e0fefa6e10314fc6f9eed127317085
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-16 17:41:45 -0300

    feat: adiciona novo exemplo na aula 02 sobre tipos primitivos

--- src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -3,6 +3,17 @@ package academy.devdojo.maratonajava;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         int idade = 18;
+        long numeroGrande = 10000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2000;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 62;
+
         System.out.println("A idade é: " + idade + " anos");
+        System.out.println(falso);
+        System.out.println(idadeByte);
     }
 }
