0da836183c1fae938d4e510e1ab270a336018d55
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-17 17:00:49 -0300

    feat: adiciona exemplo da aula 05 sobre operadores relacionais

--- src/academy/devdojo/maratonajava/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -8,5 +8,17 @@ public class Aula04Operadores {
         System.out.println(n1 - n2);
         System.out.println(n1 * n2);
         System.out.println(n1 / n2);
+
+        boolean isDezMaiorVinte = 10 > 20;
+        boolean isDezMenorVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println(isDezMaiorVinte);
+        System.out.println(isDezMenorVinte);
+        System.out.println(isDezIgualVinte);
+        System.out.println(isDezIgualDez);
+        System.out.println(isDezDiferenteDez);
     }
 }
