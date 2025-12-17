80234de653c7011894cb0c34b4db7ba3fae66570
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-17 16:25:49 -0300

    feat: adiciona exercício sobre tipos primitivos

---
+++ src/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava;
+
+/**
+ * Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+ * Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salário>, na data <data>
+ */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Miguel";
+        String endereco = "Rua Algum Lugar, n°18";
+        float salario = 5000;
+        String data = "17/12/2025";
+        System.out.println("Eu " + nome + " morando no endereço " + endereco + ", confirmo que recebi o salário de R$ " + salario + ", na data: " + data);
+    }
+}
