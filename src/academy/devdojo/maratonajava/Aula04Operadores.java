d49ec037436c863e024018dabeb80dffaff9b59d
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-17 16:47:23 -0300

    feat: adiciona exemplo da aula 04 sobre operadores aritméticos

---
+++ src/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 3;
+        System.out.println(n1 + n2);
+        System.out.println(n1 - n2);
+        System.out.println(n1 * n2);
+        System.out.println(n1 / n2);
+    }
+}
