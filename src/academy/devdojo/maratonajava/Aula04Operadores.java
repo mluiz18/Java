c6c24124d044955d6636ba4fc819a538f09eb753
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-18 15:12:40 -0300

    feat: adiciona exemplo da aula 06 sobre operadores lógicos

--- src/academy/devdojo/maratonajava/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -2,6 +2,8 @@ package academy.devdojo.maratonajava;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+
+        // Operadores Aritméticos
         int n1 = 12;
         int n2 = 3;
         System.out.println(n1 + n2);
@@ -9,6 +11,7 @@ public class Aula04Operadores {
         System.out.println(n1 * n2);
         System.out.println(n1 / n2);
 
+        // Operadores relacionais
         boolean isDezMaiorVinte = 10 > 20;
         boolean isDezMenorVinte = 10 < 20;
         boolean isDezIgualVinte = 10 == 20;
@@ -20,5 +23,13 @@ public class Aula04Operadores {
         System.out.println(isDezIgualVinte);
         System.out.println(isDezIgualDez);
         System.out.println(isDezDiferenteDez);
+
+        // Operadores Lógicos
+        int age = 29;
+        float salary = 3381F;
+        boolean isDentroDaLeiMaior30 = (age >= 30 && salary >= 4612);
+        boolean isDentroDaLeiMenor30 = (age < 30 && salary >= 3381);
+        System.out.println("isDentroDaLeiMaior30: " + isDentroDaLeiMaior30);
+        System.out.println("isDentroDaLeiMenor30: " + isDentroDaLeiMenor30);
     }
 }
