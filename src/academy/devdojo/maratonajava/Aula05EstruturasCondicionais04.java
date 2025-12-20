95f01f4179b47dd46795854e26c6ee8ffd1df20f
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-20 17:59:34 -0300

    fix: ajuste no nome da classe para padronização

--- src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais04.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava;
 
-public class Aula05EstruturaCondicionais04 {
+public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
         byte dia = 10;
 
