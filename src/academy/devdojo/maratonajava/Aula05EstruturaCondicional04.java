7a204e54e3818df1ce517559cccbbb6909e4698d
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-20 17:57:47 -0300

    feat: adiciona exemplo da aula 13 sobre a estrutura switch

---
+++ src/academy/devdojo/maratonajava/Aula05EstruturaCondicional04.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturaCondicional04 {
+    public static void main(String[] args) {
+        byte dia = 10;
+
+        switch (dia) {
+            case (1):
+                System.out.println("Domingo");
+                break;
+            case (2):
+                System.out.println("Segunda - Feira");
+                break;
+            case (3):
+                System.out.println("Terça - Feira");
+                break;
+            case (4):
+                System.out.println("Quarta - Feira");
+                break;
+            case (5):
+                System.out.println("Quinta - Feira");
+                break;
+            case (6):
+                System.out.println("Sexta - Feira");
+                break;
+            case (7):
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Valor Inválido!");
+                break;
+        }
+
+        char sexo = 'J';
+
+        switch (sexo){
+            case ('M'):
+                System.out.println("Sexo masculino!");
+                break;
+            case ('F'):
+                System.out.println("Sexo feminino!");
+                break;
+            default:
+                System.out.println("Valor inválido!");
+                break;
+        }
+    }
+}
