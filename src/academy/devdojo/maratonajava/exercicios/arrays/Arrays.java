6ab9bad728582ddc2bbf3a86c90e449fffa8f6b4
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-21 01:32:06 -0300

    feat: adiciona exercicio sobre arrays

---
+++ src/academy/devdojo/maratonajava/exercicios/arrays/Arrays.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.exercicios.arrays;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int indice = 0;
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1 - Adicionar número");
+            System.out.println("2 - Listar números");
+            System.out.println("0 - Sair");
+            int c = scn.nextInt();
+
+            switch (c) {
+                case (0):
+                    return;
+
+                case (1):
+                    System.out.print("Digite um número: ");
+                    int n = scn.nextInt();
+
+                    if (indice == arr.length) {
+                        System.out.println("Array cheio!");
+                    } else {
+                        arr[indice++] = n;
+                    }
+                    break;
+
+                case (2):
+                    for (int i = 0; i < indice; i++) {
+                        System.out.println("número na posição " + i + ": " + arr[i]);
+                    }
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+}
