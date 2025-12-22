2f9fc49cca7bf48f9eb560a6413ff8207d8def53
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-22 17:41:34 -0300

    feat: adiciona exercicio da aula 16 sobre o break

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepetição02Exercicio.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava;
+
+/**
+ * Dado o valor de um carro, descubra quantas vezes ele pode ser parcelado, aonde o valor da parcela é >= 1000
+ */
+public class Aula06EstruturasDeRepetição02Exercicio {
+    public static void main(String[] args) {
+        int valorCarro = 30000;
+
+        for (int parcela = 1; parcela < valorCarro; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000) {
+                break;
+            } else {
+                System.out.println("Parcela: " + parcela);
+            }
+
+        }
+
+    }
+}
