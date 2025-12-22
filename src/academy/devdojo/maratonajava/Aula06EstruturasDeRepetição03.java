ff0b834fa0ad04e93c4dc627f91254c2adf9e9d9
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-22 17:54:36 -0300

    feat: adiciona exemplo da aula 17 sobre o continue

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepetição03.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepetição03 {
+    public static void main(String[] args) {
+        int valorCarro = 30000;
+
+        for (int parcela = valorCarro; parcela >= 1; parcela--) {
+            double valorParcela = valorCarro/parcela;
+            if (valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Parcela: " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
