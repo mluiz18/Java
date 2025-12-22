44594e8b8f73cf6f10961f0b4a887dc742fdcc2a
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-22 17:09:27 -0300

    feat: adiciona exemplo da aula 16 sobre o break

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepetição02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepetição02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i <= 25) {
+                System.out.println(i);
+            } else {
+                break;
+            }
+
+        }
+    }
+}
