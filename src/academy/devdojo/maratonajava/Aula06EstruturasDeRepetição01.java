708e322cc259509ea9b689c2a895774b44ae70a3
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-22 16:54:04 -0300

    feat: adiciona exemplo da aula 15 sobre estruturas de repetição

---
+++ src/academy/devdojo/maratonajava/Aula06EstruturasDeRepetição01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturasDeRepetição01 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.println(count++);
+        }
+
+        count = 0;
+
+        System.out.println("Do - while");
+        do {
+            System.out.println(++count);
+        } while (count < 10);
+
+        System.out.println("for");
+        for (int i = 0; i <= 10; ++i) {
+            System.out.println(i);
+        }
+    }
+}
