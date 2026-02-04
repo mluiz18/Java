563e1a6fc1eaa9dd91bc78eb4eccaf2f8b80ed19
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-04 19:28:29 -0300

    feat: adiciona exemplo da aula 07 sobre arrays

---
+++ src/academy/devdojo/maratonajava/Aula07Arrays02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
