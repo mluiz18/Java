ba0f2cbae1190a7f0f7beef3cae105b11f6807e7
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-04 20:40:42 -0300

    feat: adiciona exemplo da aula 07 sobre arrays

---
+++ src/academy/devdojo/maratonajava/Aula07Arrays03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int[] num = new int[3];
+        int[] num2 = {1,2,3,4,5};
+        int[] num3 = new int[]{1,2,3,4,5};
+
+        for (int i : num2) {
+            System.out.println(i);
+        }
+    }
+}
