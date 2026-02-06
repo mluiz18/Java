a3d115808f5b8246c3117ee18aa6b73faaa03c76
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-05 23:35:05 -0300

    feat: adiciona exemplo da aula sobre arrays multidimensionais

---
+++ src/academy/devdojo/maratonajava/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int[][] arrm = new int[3][3];
+
+        arrm[0][0] = 1;
+        arrm[0][1] = 2;
+        arrm[0][2] = 3;
+
+        arrm[1][0] = 4;
+        arrm[1][1] = 5;
+        arrm[1][2] = 6;
+
+        arrm[2][0] = 7;
+        arrm[2][1] = 8;
+        arrm[2][2] = 9;
+
+        for (int i = 0; i < arrm.length; i++) {
+            for (int j = 0; j < arrm[i].length; j++) {
+                System.out.println(arrm[i][j]);
+            }
+        }
+    }
+
+}
