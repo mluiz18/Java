2de64579cfc551f0735e087071a58fb2b3c0c837
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-05 23:18:31 -0300

    feat: adiciona exercicio sobre inverter array

---
+++ src/academy/devdojo/maratonajava/exercicios/arrays/Arrays04.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.exercicios.arrays;
+
+import java.util.Scanner;
+
+public class Arrays04 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Digite o número para a posição " + i + ": ");
+            arr[i] = scn.nextInt();
+        }
+
+        int aux = 0;
+
+        for (int i = 0; i < (arr.length)/2 ; i++) {
+            int p = (arr.length - i) - 1;
+            aux = arr[i];
+            arr[i] = arr[p];
+            arr[p] = aux;
+        }
+
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
