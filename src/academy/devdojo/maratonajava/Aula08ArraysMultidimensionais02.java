2792b9f8c37052570ecf4e23985730110bb8e8d9
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-07 16:41:06 -0300

    feat: adiciona exemplo da aula sobre arrays multidimensionais

---
+++ src/academy/devdojo/maratonajava/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava;
+
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][];
+
+        arr[0] = new int[2];
+        arr[1] = new int[3];
+        arr[2] = new int[4];
+
+        int[][] arr2 = new int[][]{{1,2},{3,4,5},{6,7,8,9}};
+
+        for (int[] x : arr2) {
+            for (int z : x) {
+                System.out.println(z);
+            }
+        }
+    }
+}
