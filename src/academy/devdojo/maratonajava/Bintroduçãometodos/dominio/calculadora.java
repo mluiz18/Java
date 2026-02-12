eea16b0f9b8c33722b2066661f72e1e8f4c09b85
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2026-02-12 15:44:06 -0300

    feat: adição dos arquivos referente a aula sobre métodos

---
+++ src/academy/devdojo/maratonajava/Bintroduçãometodos/dominio/calculadora.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Bintroduçãometodos.dominio;
+
+public class calculadora {
+    public void calculadora() {
+        System.out.println(10 + 10);
+    }
+
+    public void sub() {
+        System.out.println(20-10);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Bintroduçãometodos/teste/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Bintroduçãometodos.teste;
+
+import academy.devdojo.maratonajava.Bintroduçãometodos.dominio.calculadora;
+
+public class CalculadoraTeste01 {
+    public static void main(String[] args) {
+        calculadora cal = new calculadora();
+        cal.calculadora();
+        cal.sub();
+    }
+}
